ec1fa0e58a3bffa84e62e9e940156692205ea01e
Aaron Goshine <greenlig@gmail.com>
2021-04-26 20:48:33 +0100

    wip: workin in container ..
      except  clicking

--- Dockerfile
+++ Dockerfile
@@ -14,14 +14,19 @@ RUN apt-get -y install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
 RUN apt-get -y install vim
 RUN apt-get -y install curl
 RUN apt-get -y install chromium-browser
-# RUN apt-get -y install google-chrome-stable
+#
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt-get -y install ./google-chrome-stable_current_amd64.deb
 
-RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-RUN apt-get install ./google-chrome-stable_current_amd64.deb
+ARG CHROME_VERSION="81.0.4044.113-1"
+RUN wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb \
+  && apt install -y /tmp/chrome.deb \
+  && rm /tmp/chrome.deb
+
+RUN wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip &&  unzip chromedriver_linux64.zip
 
 RUN mkdir -p /Home/test-folder
 ENV TD /Home/test-folder
 ADD . $TD/
 WORKDIR $TD
-# RUN npm install
-# RUN npm run e2e-test 
+ 
+
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -8,46 +8,52 @@ module.exports = {
         server_path: chromedriver.path,
         start_process: true,
         port: 4444,
-        // host: "172.17.0.1",
-      },
-desiredCapabilities: {
-        browserName: "chrome",
-        "chromeOptions": { "args": [ "--headless" ]   }
-      },
-
-      screenshots: {
-        enabled: true,
-        path: 'screenshots'
-      }
-    },
-    chromeHeadless: {
-      webdriver: {
-        server_path: chromedriver.path,
-        cli_args: ['--port=4444']
       },
       desiredCapabilities: {
         browserName: "chrome",
         "chromeOptions": {
           "args": [ "--headless" ]
+        }
+      }
+    },
+    chromeHeadless_c: {
+      webdriver: {
+        server_path: chromedriver.path,
+      },
+      desiredCapabilities: {
+        browserName: "chrome",
+        "chromeOptions": {
+          "args": [ "--headless" ],
           "binary": '/opt/google/chrome/chrome'
         }
       }
     },
-      firefoxHeadless: {
-        webdriver: {
-          server_path: geckodriver.path
-        },
-        desiredCapabilities: {
-          "browserName": "firefox",
-          "acceptInsecureCerts": true,
-          "alwaysMatch": {
-            "moz:firefoxOptions": {
-              "args": [ "-headless" ]
-            }
-          }
-
-        }
+    chromeHeadless: {
+      webdriver: {
+        server_path: chromedriver.path,
       },
+      desiredCapabilities: {
+        browserName: "chrome",
+        "chromeOptions": {
+          "args": [ "--headless" ],
+        }
+      }
+    },
+    firefoxHeadless: {
+      webdriver: {
+        server_path: geckodriver.path
+      },
+      desiredCapabilities: {
+        "browserName": "firefox",
+        "acceptInsecureCerts": true,
+        "alwaysMatch": {
+          "moz:firefoxOptions": {
+            "args": [ "-headless" ]
+          }
+        }
+
+      }
+    },
     firefox: {
       webdriver: {
         server_path: geckodriver.path,
